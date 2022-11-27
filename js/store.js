23871c7a00ea56af3e0fde2aab674707619019b0
hyerimhan <hnhyerim@gmail.com>
2022-11-27 21:31:31 +0900

    feat: Settings

---
+++ js/store.js
@@ -0,0 +1,11 @@
+export {store}
+
+const store = {
+  url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/',
+
+  headers: {
+    'content-type': 'application/json',
+    'apikey': 'FcKdtJs202209',
+    'username': 'KDT3_TEAM_NXLL'
+  },
+}
