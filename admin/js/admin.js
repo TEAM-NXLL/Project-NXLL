33e4988ba07d01c621fba325d73328ceab552a5b
DavidOH77 <gotjddl7@gmail.com>
2022-12-12 16:43:06 +0900

    Feat: create amin summary

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,6 +1,9 @@
 import { renderAllProduct } from './renderAllProducts';
 import { renderAlltransacs } from './renderAlltransacs';
+import { renderAdminSummary } from './admin-summary';
+
 (async () => {
   await renderAllProduct();
   await renderAlltransacs();
+  await renderAdminSummary();
 })();
