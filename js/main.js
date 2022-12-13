2e22bc28fc65fb9739c837f73e9f78dc611a5866
hyerimhan <hnhyerim@gmail.com>
2022-12-13 14:05:53 +0900

    feat: completed seperate keyboard, mouse, newItem section

--- js/main.js
+++ js/main.js
@@ -17,7 +17,83 @@ const root = document.querySelector('main')
 // 메인 페이지
 async function renderMain() {
   const data = await viewAllProduct();
-  root.innerHTML = mainForm(data)
+  root.innerHTML = mainForm()
+
+  const keyboardList = document.querySelector('.keyboard > .inner')
+  const mouseList = document.querySelector('.mouse > .inner')
+  const newItemList = document.querySelector('.newItem > .inner')
+
+  const keyboard = []
+  const mouse = []
+  const newItem = []
+
+  data.forEach(e => {
+    if (e['tags'] === '키보드') {
+      keyboard.push(e)
+      keyboardList.innerHTML = productList(keyboard)
+    }
+    else if (e['tags'] === '마우스') {
+      mouse.push(e)
+      mouseList.innerHTML = productList(mouse)
+    } else {
+      newItem.push(e)
+      newItemList.innerHTML = productList(newItem)
+    }
+  })
+
+  function productList(tags) {
+    
+    const colorChart = ["beige", "pastelBeige", "mint", "pink", "white", "navy", "blueNavy", "black", "green", "gray"]
+    const mainBody = []
+
+    for (let i = 0; i < tags.length; i++) {
+
+      mainBody.push(`
+        <li data-tags="${tags[i].tags}">
+          <a href="#"> 
+            <div class="imgBox">
+      `)  
+
+      mainBody.push(`
+              <img src="${tags[i].thumbnail}" alt="">
+            </div>
+          <div class="colorBox">
+      `)
+
+        const randomNum = Math.ceil(Math.random() * 5)
+        let randomIndexArray = []
+      for (let j = 0; j < randomNum; j++) {
+
+        const colorNum = Math.floor(Math.random() * 10)
+
+        if (randomIndexArray.indexOf(colorNum) === -1) {
+          randomIndexArray.push(colorNum)
+          mainBody.push(`
+                  <span class='${colorChart[colorNum]}'></span>
+              `)
+        }
+      }
+
+      const discountValue = Math.floor(((Math.random() * 9) + 1)) * 8
+
+      mainBody.push(`
+          </div >
+              <div class="textBox">
+                    ${tags[i].title} <span>B300${i}</span>
+              </div>
+              <div class="priceBox">
+                <span class="discount">
+                  ${(tags[i].price).toLocaleString()}원</span> 
+                  ${(Math.floor(Number(tags[i].price) * (100 - discountValue) / 100).toLocaleString())}원<br />
+                <span class="salePercent">${discountValue}% SALE</span>
+              </div>
+              </a>
+          </li>
+      `)
+    }
+
+    return mainBody.join('');
+  }
 
   // 메인 스와이퍼
   new Swiper('.mainSwiper', {
