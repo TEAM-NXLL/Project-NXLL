9a351a0e8fc9ce3088ec35636451b20666e56eed
quokka-eating-carrots <ovzip7@gmail.com>
2022-12-15 20:11:31 +0900

    Feat: 거래 취소, 확정 내역 구분하여 출력

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ import { doc } from "prettier";
 import { getData, getLogin, getLogOut, stateLogin, postSearch, getTransactions } from "./getdata.js";
 import { router } from "./route.js";
 import { deliveryEl, returnEl, deliveryDes, returnDes, mouseenter, mouseleave } from './footer.js'
-import { joinForm, logInForm, myOrderForm, myShoppingForm, mainForm, productList, userInfoForm, userAccountForm, detailForm, paymentForm } from "./body.js";
+import { joinForm, logInForm, myOrderForm, myShoppingForm, mainForm, productList, userInfoForm, userAccountForm, detailForm, paymentForm, myCancelOrderForm, myConfirOrderForm } from "./body.js";
 import { editUserInfo, userOwnBank, addNewAccount, choiceBank, bankChargeLookUp, ownAccountList, addAbleAccountList, cancelBank } from "./userInfo.js";
 import { viewAllProduct } from '../admin/js/requests.js'
 import { payAccountList, payBankLoopUp, buyProducts, lookProducts, cancelProduct, allCheckBox } from "./payment.js";
-import { cancelOrder, confirOrder, transLookUp } from "./myorder.js";
+import { cancelOrder, confirOrder, transLookUp, cancelOrderLookUp, confirOrderLookUp } from "./myorder.js";
 
 // 변수
 const root = document.querySelector('main');
@@ -240,13 +240,36 @@ async function renderMyShop() {
   root.innerHTML = myShoppingForm(transactions.length, total);
 }
 
+// myorder 렌더링 공통 사항
+async function listLookUp() {
+  const products = await getTransactions(localStorage.accessToken)
+  const cancels = products.filter(product => product.isCanceled === true)
+  const confirs = products.filter(product => product.done === true)
+  return { products, cancels, confirs }
+}
+
 // myorder 렌더링
-function renderMyOrder() {
-  root.innerHTML = myOrderForm();
+async function renderMyOrder() {
+  const { products, cancels, confirs } = await listLookUp()
+  root.innerHTML = myOrderForm(products.length, cancels.length, confirs.length);
   transLookUp().then(res => {
     cancelOrder()
     confirOrder()
   })
+}
+
+// myorder cancel 렌더링
+async function renderMyCancelOrder() {
+  const { products, cancels, confirs } = await listLookUp()
+  root.innerHTML = myCancelOrderForm(products.length, cancels.length, confirs.length)
+  cancelOrderLookUp()
+}
+
+// myorder confir 렌더링
+async function renderMyConfirOrder() {
+  const { products, cancels, confirs } = await listLookUp()
+  root.innerHTML = myConfirOrderForm(products.length, cancels.length, confirs.length)
+  confirOrderLookUp()
 }
 
 // userInfo 렌더링
@@ -301,5 +324,5 @@ router();
   } else return
 })();
 
-export { loginRender, joinRender, logOut, renderMyShop, renderMyOrder, renderMain, renderUserInfo, renderDetail, renderPayment }
+export { loginRender, joinRender, logOut, renderMyShop, renderMyOrder, renderMain, renderUserInfo, renderDetail, renderPayment, renderMyCancelOrder, renderMyConfirOrder }
 
