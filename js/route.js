e58a10ed4fd8d4b0dcb3976b68713edee81e324f
iziz9 <46iz.us2@gmail.com>
2022-12-19 03:35:47 +0900

    Refactor: Refactoring code in the route.js

--- js/route.js
+++ js/route.js
@@ -1,4 +1,4 @@
-import { loginRender, joinRender, renderMyShop, renderMyOrder, renderMain, renderUserInfo, renderDetail, renderPayment, renderMyCancelOrder, renderMyConfirOrder, renderCategory, cartCountCheck, sortByTag } from "./main.js"
+import { loginRender, joinRender, renderMyShop, renderMyOrder, renderMain, renderUserInfo, renderDetail, renderPayment, renderMyCancelOrder, renderMyConfirOrder, renderCategory, cartCountCheck, sortByHash } from "./main.js"
 
 export async function router() {
   cartCountCheck()
@@ -34,92 +34,18 @@ export async function router() {
   else if (routePath === '#payment') {
     renderPayment()
   }
-  else if (routePath === '#new-item') {
-    renderCategory("new-item")
+  const categoryArr = ['#new-item', '#discount', '#smart', '#notebook', '#pc', '#audio'];
+  for (let category of categoryArr) {
+    if (routePath === category) {
+      const tag = category.substring(1)
+      renderCategory(tag);
+    }
   }
-  else if (routePath === '#discount') {
-    renderCategory("discount")
-  }
-  else if (routePath === '#smart') {
-    renderCategory("smart")
-  }
-  else if (routePath === '#notebook') {
-    renderCategory("notebook")
-  }
-  else if (routePath === '#pc') {
-    renderCategory("pc")
-  }
-  else if (routePath === '#audio') {
-    renderCategory("audio")
-  }
-  else if (routePath === '#mouse') {
-    sortByTag()
-  }
-  else if (routePath === '#keyboard') {
-    sortByTag()
-  }
-  else if (routePath === '#mousepad') {
-    // renderCategory("pc")
-    sortByTag()
-  }
-  else if (routePath === '#usbhub') {
-    // renderCategory("pc")
-    sortByTag()
-  }
-  else if (routePath === '#cardreader') {
-    // renderCategory("pc")
-    sortByTag()
-  }
-  else if (routePath === '#monitorstand') {
-    // renderCategory("pc")
-    sortByTag()
-  }
-  else if (routePath === '#charging') {
-    // renderCategory("smart")
-    sortByTag()
-  }
-  else if (routePath === '#adapter') {
-    // renderCategory("smart")
-    sortByTag()
-  }
-  else if (routePath === '#smartholder') {
-    // renderCategory("smart")
-    sortByTag()
-  }
-  else if (routePath === '#smart-etc') {
-    // renderCategory("smart")
-    sortByTag()
-  }
-  else if (routePath === '#notebookstand') {
-    // renderCategory("notebook")
-    sortByTag()
-  }
-  else if (routePath === '#keypad') {
-    // renderCategory("notebook")
-    sortByTag()
-  }
-  else if (routePath === '#lock') {
-    // renderCategory("notebook")
-    sortByTag()
-  }
-  else if (routePath === '#ear-head') {
-    // renderCategory("audio")
-    sortByTag()
-  }
-  else if (routePath === '#kids') {
-    // renderCategory("audio")
-    sortByTag()
-  }
-  else if (routePath === '#mic') {
-    // renderCategory("audio")
-    sortByTag()
-  }
-  else if (routePath === '#speaker') {
-    // renderCategory("audio")
-    sortByTag()
-  }
-  else if (routePath === '#audiocable') {
-    // renderCategory("audio")
-    sortByTag()
+  const hashArr = ['#mouse', '#keyboard', '#mousepad', '#usbhub', '#cardreader', '#monitorstand', '#charging',
+  '#adapter', '#smartholder', '#smart-etc', '#notebookstand', '#keypad', '#lock', '#ear-head', '#kids', '#mic', '#speaker','#audiocable'];
+  for (let i = 0; i < hashArr.length; i +=1) {
+    if (routePath === hashArr[i]) {
+      sortByHash();
+    }
   }
 }
