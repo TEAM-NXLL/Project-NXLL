9c915be60415c28756f019da32c8167e7cc9c1ae
quokka-eating-carrots <ovzip7@gmail.com>
2022-12-29 16:29:38 +0900

    Fix: requests.js 함수 정리

--- admin/js/requests.js
+++ admin/js/requests.js
@@ -2,16 +2,22 @@ import { store } from '../../js/store.js';
 import { toast } from './toast.js';
 
 // JSON Request 양식 만들기
-export function createRequest(type, data) {
+export function createRequest(type, masterKey = true, data, accessToken) {
   const res = {
     method: type,
-    headers: { ...store.headers },
-  };
-  if (res.headers.masterKey === false) {
+    headers: { ...store.headers }
+  }
+
+  if (masterKey) {
     res.headers.masterKey = true;
   }
+
   if (data) {
     res.body = JSON.stringify(data);
+  }
+
+  if (accessToken) {
+    res.headers.Authorization = `Bearer ${accessToken}` 
   }
   return res;
 }
@@ -28,7 +34,7 @@ export async function createProduct(
   try {
     const res = await fetch(
       store.url + '/products',
-      createRequest('POST', {
+      createRequest('POST', true, {
         title,
         price,
         description,
@@ -46,9 +52,8 @@ export async function createProduct(
 // 모든 제품 조회
 export async function viewAllProduct() {
   try {
-    const res = await fetch(store.url + '/products', createRequest('GET'));
+    const res = await fetch(store.url + '/products', createRequest('GET', true));
     const getResult = await res.json();
-    // console.log(getResult);
     return getResult;
   } catch (error) {
     toast(`${error}, 잠시 후 다시 시도해주세요.`, "전체");
@@ -60,10 +65,9 @@ export async function viewAllTransactions() {
   try {
     const res = await fetch(
       store.url + '/products/transactions/all',
-      createRequest('GET'),
+      createRequest('GET', true),
     );
     const getResult = await res.json();
-    // console.log(getResult);
     return getResult;
   } catch (error) {
     toast(`${error}, 잠시 후 다시 시도해주세요.`, "거래");
@@ -75,7 +79,7 @@ export async function transactionStatus(detailId, isCanceled, done) {
   try {
     const res = await fetch(
       store.url + `/products/transactions/${detailId}`,
-      createRequest('PUT', { isCanceled, done }),
+      createRequest('PUT', true, { isCanceled, done }),
     );
     toast("거래 상태가 변경되었습니다.")
     return res;
@@ -88,7 +92,7 @@ export async function transactionStatus(detailId, isCanceled, done) {
 export async function correctProduct(productId, title, price, description, tags, thumbnailBase64, photoBase64, isSoldOut) {
   try {
     const res = await fetch(store.url + `/products/${productId}`,
-      createRequest('PUT', {
+      createRequest('PUT', true, {
         title,
         price,
         description,
@@ -109,7 +113,7 @@ export async function delProduct(productId) {
   try {
     const res = await fetch(
       store.url + `/products/${productId}`,
-      createRequest('DELETE'),
+      createRequest('DELETE', true),
     );
     return res;
   } catch (error) {
